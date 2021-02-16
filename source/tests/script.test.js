aab328e4c9211edb833a14675a0e7dda9470bfce
Ben Gu <bsgu@ucsd.edu>
2021-02-15 23:35:36 -0800

    updated test to include timer_function tests

--- source/tests/script.test.js
+++ source/tests/script.test.js
@@ -5,7 +5,7 @@ beforeEach(() => {
     jest.useFakeTimers();
 });
 
-test('Start/Stop the timer (reset function tested within)', () => {
+test('Test start/stop, reset, and basic timer function', () => {
     document.body.innerHTML = `
         <button id = "start-btn">Start</button>
         <div id="timer_display_duration">25:00</div>
@@ -23,9 +23,41 @@ test('Start/Stop the timer (reset function tested within)', () => {
     expect(start_button.innerHTML).toBe('Stop');
     expect(display.innerHTML).toBe('24:55');
 
+    // advance by 00:55
+    jest.advanceTimersByTime(55000);
+    expect(display.innerHTML).toBe('24:00');
+    
+    jest.advanceTimersByTime(1000);
+    expect(display.innerHTML).toBe('23:59');
+
+    // advance by 23:00
+    jest.advanceTimersByTime(1380000);
+    expect(display.innerHTML).toBe('0:59');
+
+    jest.advanceTimersByTime(59000);
+    expect(display.innerHTML).toBe('0:00');
+
     start_button.click();
     jest.advanceTimersByTime(100);
 
     expect(start_button.innerHTML).toBe('Start');
     expect(display.innerHTML).toBe('25:00');
 });
+
+/*
+test('Timer function decreases the time by a second each call', () => {
+
+    document.body.innerHTML = `
+        <button id = "start-btn">Start</button>
+        <div id="timer_display_duration">25:00</div>
+    `;
+
+    let time = require('../src/scripts/script');
+
+    time.timer_function();
+
+    expect(display.innerHTML).toBe('24:59');
+
+
+})
+*/ 
