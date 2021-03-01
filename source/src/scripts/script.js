d0757ad3ddb8a1fb8ae869ddcc9352108a38e55a
Yuefeng Huang <yuh035@ucsd.edu>
2021-03-01 15:50:10 -0800

    sound effect added for existing buttons; keyboard shortcut added for Starts/stop and reset

--- source/src/scripts/script.js
+++ source/src/scripts/script.js
@@ -2,14 +2,25 @@ const startButton = document.getElementById("start-btn");
 const timerDisplayDuration = document.getElementById("timer_display_duration");
 const DEFAULT_TIME = "25:00";
 const SECOND = 1000;
+let btnSound = new Audio("../icons/btnClick.mp3");
+let alarmSound = new Audio("../icons/alarm.mp3");
 let timer;
 
 startButton.onclick = startAndStopButton;
 
+window.addEventListener("keydown", function(event){
+    if(event.code=='KeyS'){
+        startAndStopButton();
+    }
+    else if(event.code=='KeyR'){
+        reset_time();
+    }
+});
 /**
  * Test header for automatic jsdoc generation
  */
 async function startAndStopButton() {
+    btnSound.play();
     if (startButton.innerHTML == "Start") {
         startButton.innerHTML = "Stop";
         timer = setInterval(timer_function, SECOND);
@@ -26,6 +37,7 @@ async function timer_function() {
     let seconds = Number(timer_text.substring(timer_text.length - 2));
 
     if (timer_text == "0:00") {
+        alarmSound.play();
         await startAndStopButton();
         return;
     }
@@ -46,5 +58,6 @@ async function timer_function() {
 }
 
 function reset_time() {
+    btnSound.play();
     timerDisplayDuration.innerHTML = DEFAULT_TIME;
 }
