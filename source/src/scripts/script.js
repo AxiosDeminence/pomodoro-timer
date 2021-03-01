d0c22033eed02e7e7c34b875532d87b2392494e5
Ben Gu <bsgu@ucsd.edu>
2021-03-01 03:23:51 -0800

    update script tests, not complee

--- source/src/scripts/script.js
+++ source/src/scripts/script.js
@@ -1,6 +1,8 @@
+import TaskItem from '../components/TaskItem';
+
 window.addEventListener('DOMContentLoaded', () => {
-    var tasks; // holds list nodes in local storage
-    var id; // id counter for task items
+    let tasks; // holds list nodes in local storage
+    let id; // id counter for task items
     if (localStorage.getItem('tasks') === null || localStorage.getItem('id') === null) {
         tasks = [];
         id = 0;
@@ -12,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
     for (let i = 0; i < tasks.length; i++) {
-        var task = new TaskItem(tasks[i]);
+        let task = new TaskItem(tasks[i]);
         document.getElementById("task-list-elements").appendChild(task);
     }
 });
@@ -21,9 +23,12 @@ window.addEventListener('DOMContentLoaded', () => {
 const popupBtn = document.getElementById('task-popup-btn');
 const popUp = document.createElement('task-popup');
 document.body.appendChild(popUp);
-popupBtn.addEventListener('click', function() {
-    popUp.shadowRoot.getElementById('add-task-popup').setAttribute('style', 'display:block');
-});
+
+if (popupBtn) {
+    popupBtn.addEventListener('click', function() {
+        popUp.shadowRoot.getElementById('add-task-popup').setAttribute('style', 'display:block');
+    });
+}
 
 // Uncomment below code to clear local storage on refresh -- Useful for debugging
 // window.onbeforeunload = function() {
--- source/tests/script.test.js
+++ source/tests/script.test.js
@@ -1,3 +1,5 @@
+const TaskItem = require('../src/components/TaskItem');
+
 beforeEach(() => {
     require('../src/scripts/script');
     localStorage.setItem('tasks', '[]');
@@ -8,29 +10,56 @@ afterEach(() => {
     localStorage.clear();
 });
 
-test('empty test', () => {
+test('Initializes localStorage correctly', () => {
     document.body.innerHTML = `
-        <button id="task-popup-btn"> <img src="../icons/plus.svg" id="plus"></button>
+        <ul id="task-list-elements">
+        </ul>      
     `;
 
-    var popupBtn = document.getElementById('task-popup-btn');
-    var popUp = document.createElement('task-popup');
-    document.body.appendChild(popUp);
+    localStorage.clear();
 
+    window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true
+    }));
+
+    expect(localStorage.getItem('tasks')).toBe('[]');
+    expect(localStorage.getItem('id')).toBe('0');
+    expect(document.getElementById("task-list-elements").children.length).toBe(0);
 });
 
-test('Pop up button works correctly', () => {
+test('Reads task list and creates tasks correctly', () => {
     document.body.innerHTML = `
-        <button id="task-popup-btn"> <img src="../icons/plus.svg" id="plus"></button>
+        <ul id="task-list-elements">
+        </ul>      
     `;
 
-    const popupBtn = document.getElementById('task-popup-btn');
-    const popUp = document.createElement('task-popup');
-    document.body.appendChild(popUp);
+    localStorage.setItem('tasks', "[{\"id\":\"0\",\"checked\":false,\"text\":\"test_task\"}]");
 
-    popupBtn.click();
+    window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true
+    }));
 
-    expect(popUp.shadowRoot.querySelector('add-task-popup').getAttribute('style')).toBe('display:block');
-
-
+    expect(document.getElementById("task-list-elements").children.length).toBe(1);
+    
+    // let tasks = JSON.parse(localStorage.getItem('tasks'));
+    // let taskItem = new TaskItem(tasks[0]);
+    // expect(document.getElementById("task-list-elements").children[0]).toBe(taskItem);
 });
+
+// test('Pop up button works correctly', () => {
+//     document.body.innerHTML = `
+//         <button id="task-popup-btn"> <img src="../icons/plus.svg" id="plus"></button>
+//     `;
+
+//     const popupBtn = document.getElementById('task-popup-btn');
+//     const popUp = document.createElement('task-popup');
+//     document.body.appendChild(popUp);
+
+//     popupBtn.click();
+
+//     expect(popUp.shadowRoot.querySelector('add-task-popup').getAttribute('style')).toBe('display:block');
+
+
+// });
