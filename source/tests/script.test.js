180a6451e8d925d57a4d1c85c78e0db55737ddff
Ben Gu <bsgu@ucsd.edu>
2021-03-02 00:00:24 -0800

    update script.test, but issue remains

--- source/tests/script.test.js
+++ source/tests/script.test.js
@@ -1,5 +1,6 @@
 beforeEach(() => {
     require('../src/scripts/script');
+    require('../src/components/TaskItem');
     localStorage.setItem('tasks', '[]');
     localStorage.setItem('id', '0');
     document.body.innerHTML = `
@@ -25,7 +26,9 @@ test('Initializes localStorage correctly', () => {
     expect(document.getElementById("task-list-elements").children.length).toBe(0);
 });
 
-test('Reads task list and creates tasks correctly', () => {
+
+// taskItem.checked and taskItem.text are undefined, but taskItem.id is set properly...
+test('Reads task list and creates one task correctly', () => {
     localStorage.setItem('tasks', "[{\"id\":\"0\",\"checked\":false,\"text\":\"test_task\"}]");
 
     window.document.dispatchEvent(new Event("DOMContentLoaded", {
@@ -37,8 +40,32 @@ test('Reads task list and creates tasks correctly', () => {
     
     let taskItem = document.getElementById("task-list-elements").children[0];
     // expect(taskItem).toBe("");
-    expect(taskItem.checked).toBe("false");
+    // expect(taskItem.checked).toBe("false");
     expect(taskItem.id).toBe("0");
-    expect(taskItem.text).toBe("test_task");
+    // expect(taskItem.text).toBe("test_task");
 });
 
+test('Reads task list and creates multiple tasks correctly', () => {
+    localStorage.setItem('tasks', "[{\"id\":\"0\",\"checked\":false,\"text\":\"test_task\"},"
+    + "{\"id\":\"1\",\"checked\":false,\"text\":\"test_task1\"}]");
+
+    window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true
+    }));
+
+    expect(document.getElementById("task-list-elements").children.length).toBe(2);
+    
+    let taskItem = document.getElementById("task-list-elements").children[0];
+    // expect(taskItem).toBe("");
+    // expect(taskItem.checked).toBe("false");
+    expect(taskItem.id).toBe("0");
+    // expect(taskItem.text).toBe("test_task");
+
+    let taskItem1 = document.getElementById("task-list-elements").children[1];
+    // expect(taskItem1).toBe("");
+    // expect(taskItem1.checked).toBe("false");
+    expect(taskItem1.id).toBe("1");
+    // expect(taskItem1.text).toBe("test_task1");
+
+});
