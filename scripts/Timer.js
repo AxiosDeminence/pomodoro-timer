20cb9e587b926ceb0860bd2e416ccd77105edef8
nxbt <nxbt1998@gmail.com>
2021-03-16 05:29:22 +0000

    Deploying to gh-pages from @ Jackie1342/cse110-w21-group13@1bfd6b25b89b1ebd56793b3c76633a29788db2d0 🚀

---
+++ scripts/Timer.js
@@ -0,0 +1,100 @@
+const startButton = document.getElementById('start-btn');
+const timerDisplayDuration = document.getElementById('timer_display_duration');
+let timer;
+let timerStatus = 'pomo';
+// let pomoTime = localStorage.getItem('pomo-length');
+// let breakTime = localStorage.getItem('short-break-length');
+// let longBreakTime = localStorage.getItem('long-break-length');
+let breakCounter = 0;
+const SECOND = 1000;
+const LIGHT_COLOR = '#f3606060';
+const DARK_COLOR = '#f36060';
+
+if (localStorage.getItem('pomo-length') === null) {
+    localStorage.setItem('pomo-length', '25');
+    localStorage.setItem('short-break-length', '5');
+    localStorage.setItem('long-break-length', '15');
+}
+let pomoTime = localStorage.getItem('pomo-length');
+let breakTime = localStorage.getItem('short-break-length');
+let longBreakTime = localStorage.getItem('long-break-length');
+
+timerDisplayDuration.innerHTML = `${pomoTime}:00`;
+
+function switchMode() {
+    const pomoButton = document.getElementById('pomo-btn');
+    const breakButton = document.getElementById('break-btn');
+    if (timerStatus === 'pomo' && breakCounter >= 3) {
+        timerDisplayDuration.innerHTML = `${longBreakTime}:00`;
+        pomoButton.style.backgroundColor = LIGHT_COLOR;
+        breakButton.style.backgroundColor = DARK_COLOR;
+        timerStatus = 'break';
+        breakCounter = 0;
+    } else if (timerStatus === 'pomo') {
+        timerDisplayDuration.innerHTML = `${breakTime}:00`;
+        pomoButton.style.backgroundColor = LIGHT_COLOR;
+        breakButton.style.backgroundColor = DARK_COLOR;
+        timerStatus = 'break';
+        breakCounter += 1;
+    } else {
+        timerDisplayDuration.innerHTML = `${pomoTime}:00`;
+        pomoButton.style.backgroundColor = DARK_COLOR;
+        breakButton.style.backgroundColor = LIGHT_COLOR;
+        timerStatus = 'pomo';
+    }
+}
+
+async function timerFunction() {
+    let timerText = timerDisplayDuration.innerHTML;
+
+    if (timerText === '0:00') {
+        switchMode();
+        timerText = timerDisplayDuration.innerHTML;
+    }
+
+    let minutes = Number(timerText.substring(0, timerText.length - 3));
+    let seconds = Number(timerText.substring(timerText.length - 2));
+
+    if (!(seconds === 0)) {
+        seconds -= 1;
+    } else {
+        seconds = 59;
+        minutes -= 1;
+    }
+
+    if (seconds < 10) {
+        seconds = `0${String(seconds)}`;
+    }
+
+    timerDisplayDuration.innerHTML = `${minutes}:${seconds}`;
+}
+
+async function start() {
+    startButton.innerHTML = 'Stop';
+    timer = setInterval(timerFunction, SECOND);
+}
+
+function resetTimer() {
+    timerDisplayDuration.innerHTML = `${pomoTime}:00`;
+    timerStatus = 'pomo';
+}
+
+async function stop() {
+    pomoTime = localStorage.getItem('pomo-length');
+    breakTime = localStorage.getItem('short-break-length');
+    longBreakTime = localStorage.getItem('long-break-length');
+    clearInterval(timer);
+    setTimeout(resetTimer, SECOND / 10);
+    breakCounter = 0;
+    startButton.innerHTML = 'Start';
+}
+
+async function startAndStopButton() {
+    if (startButton.innerHTML === 'Start') {
+        start();
+    } else {
+        stop();
+    }
+}
+
+startButton.addEventListener('click', startAndStopButton);
---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// require('../components/TaskItem');
+
+// const TaskItem = require('../components/TaskItem');
+
+window.addEventListener('DOMContentLoaded', () => {
+    let tasks; // holds list nodes in local storage
+    let id; // id counter for task items
+
+    if (localStorage.getItem('tasks') === null || localStorage.getItem('id') === null) {
+        tasks = [];
+        id = 0;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.setItem('id', `${id}`);
+        // console.log('tasks:',localStorage.getItem('tasks'),'\nid:',localStorage.getItem('id'));
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    for (let i = 0; i < tasks.length; i += 1) {
+        const task = document.createElement('task-item');
+        task.setAttribute('id', tasks[i].id);
+        task.setAttribute('checked', tasks[i].checked);
+        task.setAttribute('text', tasks[i].text);
+        // new TaskItem(tasks[i]);
+        document.getElementById('task-list-elements').appendChild(task);
+    }
+});
+// Uncomment below code to clear local storage on refresh -- Useful for debugging
+// window.onbeforeunload = function() {
+//     // localStorage.removeItem('tasks');
+//     localStorage.clear();
+//     return '';
+// };
