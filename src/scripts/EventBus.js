64e466a4f6613b0afc85e7142e4d7516e44d092d
Juhmer Tena <juhmer@att.net>
2022-07-08 15:38:23 -0700

    Create EventBus for pubsub pattern

--- src/scripts/EventBus.js
+++ src/scripts/EventBus.js
@@ -6,16 +6,80 @@
  * @module EventBus.js
  */
 
-class EventBus {
-    #observers;
+/**
+ * EventBus assumes will manage unsubscribing before event targets would disappear.
+ */
+export default class EventBus {
+    constructor() {
+        /** @type {Map<string, Set<number>>} */
+        const subscriptions = new Map();
 
-    /**
-     * Adds a listener for an event dispatched to the EventBus.
-     * @param {HTMLElement} elem - Element that listens for the event
-     * @param {string} eventType - Event to be emitted to the element
-     * @param {function} cb - Callback to be done for the event
-     */
-    listen(elem, eventType, cb) {
-        this.#observers.push(new MutationObserver)
+        /** @type {Map<number, function>} */
+        const callbacks = new Map();
+
+        /** @type {number} */
+        let subscriberUID = -1;
+
+        /**
+         * Adds a subscriber for an event dispatched through the EventBus.
+         * @param {string} eventType Event the callback is now listening for
+         * @param {function} cb Callback when the event is published
+         * @return {number} uuid for the subscription
+         */
+        this.subscribe = (eventType, cb) => {
+            // Prevent non-callbacks from being added
+            if (typeof cb !== 'function') {
+                throw new TypeError('Callback must be a function');
+            }
+            subscriberUID += 1;
+
+            if (!subscriptions.has(eventType)) {
+                subscriptions.set(eventType, new Set());
+            }
+            subscriptions.get(eventType).add(subscriberUID);
+
+            callbacks.set(subscriberUID, cb);
+
+            return subscriberUID;
+        };
+
+        /**
+         * Removes a subscriber for an event dispatched through the EventBus.
+         * @param {number} uid Token for the subscriber to be removed
+         */
+        this.unsubscribe = (uid) => {
+            // If already unsubscribed or invalid uid
+            if (!callbacks.has(uid)) {
+                return;
+            }
+            callbacks.delete(uid);
+
+            Array.from(subscriptions.values()).every((subscriberSet) => {
+                if (subscriberSet.has(uid)) {
+                    subscriberSet.delete(uid);
+                    // Break out of the loop
+                    return false;
+                }
+
+                return true;
+            });
+        };
+
+        /**
+         * Publish an event dispatched to the EventBus to all subscribers.
+         * @param {string} eventType
+         * @param {...any} args
+         */
+        this.publish = (eventType, ...args) => {
+            // If the event has no listeners, don't publish it but begin preparation for subscribers
+            if (!subscriptions.has(eventType)) {
+                subscriptions.set(eventType, new Set());
+                return;
+            }
+
+            const subscribers = subscriptions.get(eventType);
+
+            subscribers.forEach((uid) => callbacks.get(uid)(...args));
+        };
     }
 }
---
+++ src/scripts/EventBus.test.js
@@ -0,0 +1,109 @@
+// @ts-check
+
+import EventBus from './EventBus';
+
+const SAMPLE_EVENT_1 = 'happy';
+const SAMPLE_EVENT_2 = 'sad';
+const UNUSED_EVENT = 'mad';
+
+describe('Token behavior', () => {
+    const pubsub = new EventBus();
+
+    it('Subscriptions are bound to a token', () => {
+        expect(pubsub.subscribe(SAMPLE_EVENT_1, () => { })).toBe(0);
+    });
+
+    it('Each token is unique', () => {
+        expect(pubsub.subscribe(SAMPLE_EVENT_2, () => { })).toBe(1);
+    });
+});
+
+describe('Pub/sub behavior', () => {
+    /** @type {EventBus} */
+    let pubsub;
+
+    /** @type {any[]} */
+    let passedArgs;
+
+    const spy = jest.fn((...args) => { passedArgs = args; });
+
+    beforeEach(() => {
+        spy.mockClear();
+        pubsub = new EventBus();
+        passedArgs = undefined;
+    });
+
+    it('Subscriptions must provide a valid callback', () => {
+        expect(pubsub.subscribe.bind(null, SAMPLE_EVENT_1, 1))
+            .toThrowError(TypeError);
+    });
+
+    it('Single subscription gets notified', () => {
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.publish(SAMPLE_EVENT_1);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('Multiple subscriptions get notified', () => {
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.publish(SAMPLE_EVENT_1);
+        expect(spy).toHaveBeenCalledTimes(2);
+    });
+
+    it('Subscriptions are scoped to specific events', () => {
+        pubsub.publish(UNUSED_EVENT, spy);
+        expect(spy).toHaveBeenCalledTimes(0);
+
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.subscribe(SAMPLE_EVENT_2, spy);
+        pubsub.publish(SAMPLE_EVENT_1);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('Subscriptions can be removed', () => {
+        const uid = pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.unsubscribe(uid);
+
+        pubsub.publish(SAMPLE_EVENT_1);
+        expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    it('Subscription removal is idempotent', () => {
+        const uid = pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.unsubscribe(uid);
+        // Does not throw
+        pubsub.unsubscribe(uid);
+    });
+
+    it('Unsubscribing does not remove other subscriptions for same event', () => {
+        const uid = pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+
+        pubsub.unsubscribe(uid);
+        pubsub.publish(SAMPLE_EVENT_1);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('Unsubscribing does not remove other subscriptions for different event', () => {
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        const uid = pubsub.subscribe(SAMPLE_EVENT_2, spy);
+
+        pubsub.unsubscribe(uid);
+
+        pubsub.publish(SAMPLE_EVENT_1);
+        pubsub.publish(SAMPLE_EVENT_2);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('Publishing passes arguments', () => {
+        const args = ['a', 'b', 'c'];
+        pubsub.subscribe(SAMPLE_EVENT_1, spy);
+        pubsub.publish(SAMPLE_EVENT_1, ...args);
+
+        expect(args).toEqual(passedArgs);
+    });
+});
